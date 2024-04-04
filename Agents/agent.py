ec779a0f778b111735654541b13856bb96e9edf5
Youness <youness.elbrag@aigot.com>
2024-04-04 00:08:53 +0000

    building the agent LLm used ollam model llama2

--- Agents/agent.py
+++ Agents/agent.py
@@ -1,28 +1,24 @@
+from langchain.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 from langchain.memory import ConversationBufferMemory
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.prompts import MessagesPlaceholder
 from langchain.sql_database import SQLDatabase
 from langchain.agents import initialize_agent, AgentType
 from langchain.prompts import PromptTemplate
-from langchain.chat_models import ChatOpenAI, ChatCohere
 from tools.tools import query_from_vdb, query_from_sql
 from Helpers.helper_functions import tool_initializer
 import json
 import os
 
-
 sql_tool = query_from_sql()
-
 
 def run_robofund_agent(query: str) -> str:
     system_prompt = ""
     with open("Prompts\intermediate_agent_template.json", "r") as file:
         system_prompt = json.load(file)
-    llm = ChatOpenAI(
-        openai_api_key=os.environ.get("OPENAI_API_KEY"),
-        temperature=0,
-        model="gpt-3.5-turbo",
-        model_kwargs={"stop": ["\Observation:"]},
-    )
+
+    llm = ChatOllama(model="llama2")  # Initialize ChatOllama with the "llama2" model
 
     template = system_prompt[0]["template"]
 
@@ -50,7 +46,6 @@ def run_robofund_agent(query: str) -> str:
         verbose=True,
         handle_parsing_errors=True,
         early_stopping_method="generate",
-        
     )
 
     prompt_template = PromptTemplate(template=template, input_variables=["question"])
